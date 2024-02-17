6897fb06f78ec7d0c57e55fdf255633fa11d8114
Anas Rana <130040052+anasrana162@users.noreply.github.com>
2024-02-17 18:35:12 +0500

    added ui

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className=' flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 '>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>Login</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Login
